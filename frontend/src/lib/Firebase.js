4a349342f26c71b1c3ac3efb80399232a8855c06
Md. Jahid Hasan Jitu <jahidjitu095@gmail.com>
2026-02-28 00:03:29 +0600

    vite update

--- frontend/src/lib/Firebase.js
+++ frontend/src/lib/Firebase.js
@@ -8,12 +8,12 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.REACT_FIREBASE_API_KEY,
-  authDomain: import.meta.env.REACT_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.REACT_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.REACT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_FIREBASE_MESSEGING_SENDER,
-  appId: import.meta.env.REACT_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSEGING_SENDER,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
